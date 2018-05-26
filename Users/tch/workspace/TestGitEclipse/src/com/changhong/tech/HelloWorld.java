1ad1bf0be1f29bac37b46bd285b6ae6a2afb12ae
tomtiangithub <984803465@qq.com>
2018-05-26 12:37:45 +0800

    eee

--- Users/tch/workspace/TestGitEclipse/src/com/changhong/tech/HelloWorld.java
+++ Users/tch/workspace/TestGitEclipse/src/com/changhong/tech/HelloWorld.java
@@ -3,7 +3,7 @@ package com.changhong.tech;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	  System.out.println("1223334444");
+	  System.out.println("12233344445555");
 
 	}
 
